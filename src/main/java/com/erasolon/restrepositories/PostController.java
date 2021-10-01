992600deed819bf7d726b5446b4720c16429b31e
Emile <emilerasolo@yahoo.com>
2021-10-01 05:46:42 +0200

    reactive-spring-mongodb

--- src/main/java/com/erasolon/restrepositories/PostController.java
+++ src/main/java/com/erasolon/restrepositories/PostController.java
@@ -4,7 +4,6 @@ import com.erasolon.documents.Post;
 import com.erasolon.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
